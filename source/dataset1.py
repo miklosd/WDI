2267372869c45fc3adb079c50662d816bc0260a2
miklosd <54479098+miklosd@users.noreply.github.com>
2019-09-01 21:54:28 +0200

    Update dataset1.py

--- source/dataset1.py
+++ source/dataset1.py
@@ -7,7 +7,7 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-dfCountry = pd.read_csv('data/worldbank/WDI/WDICountry.csv', delimiter=',')
+dfCountry = pd.read_csv('https://raw.githubusercontent.com/miklosd/WDI/master/data/WDICountry.csv', delimiter=',')
 countryFilter = dfCountry[dfCountry['Currency Unit'] == 'Euro']['Country Code']
 
 
@@ -15,7 +15,7 @@ yearIndex = []
 for i in range(1960,2018):
     yearIndex.append(str(i))
 
-dfData = df = pd.read_csv('data/worldbank/WDI/WDIData.csv', delimiter=',')
+dfData = df = pd.read_csv('https://raw.githubusercontent.com/miklosd/WDI/master/data/WDIData.csv', delimiter=',')
 
 dfResult = pd.DataFrame(index = yearIndex, columns = ['Year'])
 dfResult['Year'] = yearIndex
